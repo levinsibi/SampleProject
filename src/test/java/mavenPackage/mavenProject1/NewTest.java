5e45f2657ae6ed2c7d1eb610ad943ba09e761f2f
X163459 <X163459@TRPRDK67HF1J-D.hrbinc.hrblock.net>
2018-08-29 10:29:22 +0530

    testing

---
+++ src/test/java/mavenPackage/mavenProject1/NewTest.java
@@ -0,0 +1,28 @@
+package mavenPackage.mavenProject1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  driver.get("https://www.google.com/");
+	  driver.manage().window().maximize();
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\jars\\chromedriver.exe");
+	 driver=new ChromeDriver();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
---
+++ src/test/java/mavenPackage/mavenProject1/test.java
@@ -0,0 +1,18 @@
+package mavenPackage.mavenProject1;
+
+import javax.sql.rowset.WebRowSet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("helloo");
+		WebDriver driver=new FirefoxDriver();
+	
+	}
+
+}
